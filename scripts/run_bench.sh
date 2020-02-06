979009c5bb8fe5e504b240999e2724b1ed3d3fe3
Kotaro Inoue <kinoue@yama.info.waseda.ac.jp>
2020-02-07 06:19:19 +0900

    fix bench script

--- scripts/run_bench.sh
+++ scripts/run_bench.sh
@@ -8,4 +8,4 @@ FNAME=$(date "+%Y%m%d-%H%M%S").log
 
 mkdir -p $LOGPATH
 
-numactl --physcpubind=0 --membind=1 $ROOTPATH/bin/sample &> $LOGPATH/$FNAME
+numactl --physcpubind=0 --membind=1 $ROOTPATH/bin/bench &> $LOGPATH/$FNAME
